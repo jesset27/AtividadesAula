9fa6b4c6e787cf529e03f937ba7ad0406e28be23
Jessé Willian <90875367+jesset27@users.noreply.github.com>
2023-03-14 17:11:03 -0300

    1

--- Aula3/exe1/index.js
+++ Aula3/exe1/index.js
@@ -2,24 +2,42 @@ const array = new Array();
 const ul = document.querySelector('ul');
 let c = 0;
 
-
-
-function addElemento(array){
+function addElemento(array) {
     ul.innerHTML = null;
-    for (let i = 0; i < array.length; i++){
-        const addElemento = document.createElement('li');
-        const button = document.createElement('button');
-        addElemento.id = c;
-        c++;
-        ul.appendChild(addElemento);
-        addElemento.innerHTML = array[i] + ' ';
-        button.innerHTML = 'apagar';
-        addElemento.appendChild(button);   
+    for (let i = 0; i < array.length; i++) {
+      const addElemento = document.createElement('li');
+      const buttonUp = document.createElement('button');
+      const buttonDown = document.createElement('button');
+      addElemento.id = c;
+      c++;
+      ul.appendChild(addElemento);
+      addElemento.innerHTML = array[i] + ' ';
+      buttonUp.innerHTML = '↑';
+      buttonUp.addEventListener('click', () => {
+        if (i > 0) {
+          [array[i], array[i-1]] = [array[i-1], array[i]];
+          addElemento.parentNode.insertBefore(addElemento, addElemento.previousSibling);
+        }
+      });
+      addElemento.appendChild(buttonUp);
+      buttonDown.innerHTML = '↓';
+      buttonDown.addEventListener('click', () => {
+        if (i < array.length-1) {
+          [array[i], array[i+1]] = [array[i+1], array[i]];
+          addElemento.parentNode.insertBefore(addElemento.nextSibling, addElemento);
+        }
+      });
+      addElemento.appendChild(buttonDown);
+      const button = document.createElement('button');
+      button.innerHTML = 'apagar';
+      button.addEventListener('click', () => {
+        array.splice(i, 1);
+        addElemento.remove();
+      });
+      addElemento.appendChild(button);
     }
-}
-
-
-
+  }
+  
 
 document.querySelector('button').addEventListener('click', ()=>{
     const input = document.querySelector('#value').value;
